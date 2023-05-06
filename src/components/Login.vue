bd020854856f19fe470108ac527478a6b58ed0e7
冰鲜柠檬水 <467028162@qq.com>
2023-05-06 17:19:55 +0800

    登录页进行中，并提交到分支

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+      <!-- 登陆表单区域 -->
+      <el-form label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input></el-input>
+        </el-form-item>
+      </el-form>
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+  height: 100vh;
+  position: relative;
+}
+.login_box {
+  height: 300px;
+  width: 450px;
+  background-color: #fff;
+  border-radius: 10px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  .avatar_box {
+    width: 130px;
+    height: 130px;
+    border: 1px solid #eee;
+    border-radius: 50%;
+    padding: 10px;
+    box-shadow: 0 0 10px #eee;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: #fff;
+    img {
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      background-color: #eee;
+    }
+  }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import router from '@/router';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+// 导入全局样式表
+import './assets/css/global.css'
 
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
+Vue.use(ElementUI)
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
